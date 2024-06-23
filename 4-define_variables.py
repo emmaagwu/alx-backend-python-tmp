e3bee215c9ebdff200c7bcefe7232183ca684f5b
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-06-23 16:25:50 +0100

    Add variable definitions

---
+++ 4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module defines variables of different types.
+
+Variables:
+- a: An integer variable with a value of 1.
+- pi: A float variable with a value of 3.14.
+- i_understand_annotations: A boolean variable with a value of True.
+- school: A string variable with a value of 'Holberton'.
+"""
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
